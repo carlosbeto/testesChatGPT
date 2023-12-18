59b9f85dc2135ee8c64c46bb7b09a12308e3efed
carlosbeto <betocac@gmail.com>
2023-12-18 10:28:35 -0300

    'Revisão módulo 16'

--- modulo15_OO/bibliotecaMatplotlib1.py
+++ modulo15_OO/bibliotecaMatplotlib1.py
@@ -1,7 +1,7 @@
 vendas_meses = [1500, 1727, 1350, 999, 1050, 1027,1022, 1500, 2000, 2362, 2100, 2762]
 meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
 
-#plotar o gráfico da forma mais cimples
+#plotar o gráfico da forma mais simples
 import matplotlib.pyplot as plt
 
 plt.plot(meses, vendas_meses, color='red')
--- modulo15_OO/bibliotecaTime.py
+++ modulo15_OO/bibliotecaTime.py
@@ -1,5 +1,6 @@
 import time
 data_atual = time.ctime()
 segundos_hoje = time.time()
+
 print('Data de hoje.: ', data_atual)
 print('segundos hoje.: ',segundos_hoje)
