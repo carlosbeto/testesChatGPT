27a6c4b151273845e5760e3ee62decdbff243ffe
carlosbeto <betocac@gmail.com>
2024-01-10 09:46:45 -0300

    Teste retoro férias

--- modulo23_usandoBibiliotecas/ex02_usandoRequests.py
+++ modulo23_usandoBibiliotecas/ex02_usandoRequests.py
@@ -1,5 +1,6 @@
+
 import pandas as pd
 
-tabela = pd.read_excel("C:\Users\ca049341\Documents\MEUS DOCS\Python-Impressionador-Hashteag\util\Contratos.xlsx")
+tabela = pd.read_excel("C:/Users/ca049341/Documents/MEUS DOCS/Python-Impressionador-Hashteag/util/Contratos.xlsx")
 
 tabela.dtypes
